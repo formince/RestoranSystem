6f0d02ff0ede94577ca3ef540fc689b2bde709e1
yunusozyesil <yunus.ozyesil@betabt.com>
2026-01-20 12:44:07 +0300

    sqrcit değişti

--- DbScripts/pre-script.sql
+++ DbScripts/pre-script.sql
@@ -1,1 +1,19 @@
-INSERT INTO LogTablosu (Mesaj) VALUES ('Pre-Script çalýþtý');
+-- GitHub Action Testi için geçici veri ekleme
+INSERT INTO Categories (
+    Name, 
+    Description, 
+    DisplayOrder, 
+    ImageUrl, 
+    CreatedDate, 
+    UpdatedDate, 
+    IsActive
+) 
+VALUES (
+    'GitHub Test Kategorisi', 
+    'Bu kayýt otomatik deploy testi sýrasýnda eklendi.', 
+    999, -- En sona atsýn diye yüksek numara verdik
+    'no-image.jpg', 
+    GETDATE(), 
+    GETDATE(), 
+    1
+);
